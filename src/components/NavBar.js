957359f6b277d03a74f6a80b043a848deffa8294
gSwapDev <mydevtest96@gmail.com>
2021-06-28 19:33:54 +0200

    Comminting

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -30,6 +30,153 @@ import pug from "./assets/gangster.png";
 // if (dif > 0) {
 //   openContest_0 = true;
 // }
+const tokenABI = [
+  {
+    name: "Transfer",
+    inputs: [
+      { type: "address", name: "_from", indexed: true },
+      { type: "address", name: "_to", indexed: true },
+      { type: "uint256", name: "_value", indexed: false },
+    ],
+    anonymous: false,
+    type: "event",
+  },
+  {
+    name: "Approval",
+    inputs: [
+      { type: "address", name: "_owner", indexed: true },
+      { type: "address", name: "_spender", indexed: true },
+      { type: "uint256", name: "_value", indexed: false },
+    ],
+    anonymous: false,
+    type: "event",
+  },
+  {
+    name: "__init__",
+    outputs: [],
+    inputs: [
+      { type: "bytes32", name: "_name" },
+      { type: "bytes32", name: "_symbol" },
+      { type: "uint256", name: "_decimals" },
+      { type: "uint256", name: "_supply" },
+    ],
+    constant: false,
+    payable: false,
+    type: "constructor",
+  },
+  {
+    name: "deposit",
+    outputs: [],
+    inputs: [],
+    constant: false,
+    payable: true,
+    type: "function",
+    gas: 74279,
+  },
+  {
+    name: "withdraw",
+    outputs: [{ type: "bool", name: "out" }],
+    inputs: [{ type: "uint256", name: "_value" }],
+    constant: false,
+    payable: false,
+    type: "function",
+    gas: 108706,
+  },
+  {
+    name: "totalSupply",
+    outputs: [{ type: "uint256", name: "out" }],
+    inputs: [],
+    constant: true,
+    payable: false,
+    type: "function",
+    gas: 543,
+  },
+  {
+    name: "balanceOf",
+    outputs: [{ type: "uint256", name: "out" }],
+    inputs: [{ type: "address", name: "_owner" }],
+    constant: true,
+    payable: false,
+    type: "function",
+    gas: 745,
+  },
+  {
+    name: "transfer",
+    outputs: [{ type: "bool", name: "out" }],
+    inputs: [
+      { type: "address", name: "_to" },
+      { type: "uint256", name: "_value" },
+    ],
+    constant: false,
+    payable: false,
+    type: "function",
+    gas: 74698,
+  },
+  {
+    name: "transferFrom",
+    outputs: [{ type: "bool", name: "out" }],
+    inputs: [
+      { type: "address", name: "_from" },
+      { type: "address", name: "_to" },
+      { type: "uint256", name: "_value" },
+    ],
+    constant: false,
+    payable: false,
+    type: "function",
+    gas: 110600,
+  },
+  {
+    name: "approve",
+    outputs: [{ type: "bool", name: "out" }],
+    inputs: [
+      { type: "address", name: "_spender" },
+      { type: "uint256", name: "_value" },
+    ],
+    constant: false,
+    payable: false,
+    type: "function",
+    gas: 37888,
+  },
+  {
+    name: "allowance",
+    outputs: [{ type: "uint256", name: "out" }],
+    inputs: [
+      { type: "address", name: "_owner" },
+      { type: "address", name: "_spender" },
+    ],
+    constant: true,
+    payable: false,
+    type: "function",
+    gas: 1025,
+  },
+  {
+    name: "name",
+    outputs: [{ type: "bytes32", name: "out" }],
+    inputs: [],
+    constant: true,
+    payable: false,
+    type: "function",
+    gas: 723,
+  },
+  {
+    name: "symbol",
+    outputs: [{ type: "bytes32", name: "out" }],
+    inputs: [],
+    constant: true,
+    payable: false,
+    type: "function",
+    gas: 753,
+  },
+  {
+    name: "decimals",
+    outputs: [{ type: "uint256", name: "out" }],
+    inputs: [],
+    constant: true,
+    payable: false,
+    type: "function",
+    gas: 783,
+  },
+];
 
 class NavBar extends Component {
   async componentWillMount() {
@@ -45,6 +192,10 @@ class NavBar extends Component {
       const web3 = new Web3(window.ethereum);
       const netId = await web3.eth.net.getId();
       const blockAc = await web3.eth.getBlockNumber();
+      const pugContract = new web3.eth.Contract(
+        tokenABI,
+        "0x59b6196e41c118dfF75961257b882e86b915a0e8"
+      );
 
       console.log(netId);
       console.log(`Block: ${blockAc}`);
@@ -55,10 +206,15 @@ class NavBar extends Component {
       //load balance
       if (typeof accounts[0] !== "undefined") {
         const balance = await web3.eth.getBalance(accounts[0]);
+        const pugBalance = await pugContract.methods
+          .balanceOf(this.state.account)
+          .call();
+
         this.setState({
           account: accounts[0],
           balance: web3.utils.fromWei(balance),
           web3: web3,
+          pugBalance: pugBalance,
         });
       } else {
         window.alert("Please login with MetaMask");
@@ -95,6 +251,7 @@ class NavBar extends Component {
       account: "",
       web3: "undefined",
       page: "about",
+      pugBalance: 0,
     };
   }
 
@@ -170,12 +327,20 @@ class NavBar extends Component {
                 <b>
                   <font color="ec6998">PUG:</font>
                 </b>{" "}
-                0,0017
+                0,0020
                 <b>$</b>
               </font>
             </div>
           </div>
+
           <div class="account-button no-select"></div>
+        </div>
+        <div>
+          <span>
+            <font color="ec6998"> M</font>
+            <font color="white">y PUG</font>
+            <div>{this.state.pugBalance}</div>
+          </span>
         </div>
         <a>
           {" "}
